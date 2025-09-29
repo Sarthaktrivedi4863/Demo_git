ee6a9b34abf9bb37256dfc4f036358c3290e8b9b
Sarthaktrivedi4863 <trivedisarthak01@gmail.com>
2025-09-29 23:32:33 +0530

    code

---
+++ string_compression.java
@@ -0,0 +1,44 @@
+import java.lang.reflect.Array;
+import java.util.*;
+public class string_compression {
+    public static void main(String[] args) {
+        
+        Scanner sc = new Scanner(System.in);
+        String demo=sc.nextLine();
+
+        ArrayList<Character> al = new ArrayList<>();
+        ArrayList<Character> cl = new ArrayList<>();
+        
+        HashSet<Character> hs = new HashSet<>();
+        int n = demo.length();
+        for(int i=0;i<n;i++)
+        if(!hs.contains(demo.charAt(i)))
+            {
+                hs.add(demo.charAt(i));
+                al.add(demo.charAt(i));
+            }
+        
+        int uele=hs.size();
+        for(int i=0;i<n;i++)
+            cl.add(demo.charAt(i));
+        
+        int [] arr = new int[uele];
+        
+            for(int i=0;i<uele;i++)
+            {
+                arr[i]=0;
+                for(int j=0;j<n;j++)
+                {
+                    if(al.get(i)==cl.get(j))
+                        arr[i]++;
+                }
+            }
+
+            System.out.println(Arrays.toString(arr));
+
+
+    
+
+    }
+    
+}

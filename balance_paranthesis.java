a57b2218536430ccea311b5a2bfe5258d5d8cf99
Sarthaktrivedi4863 <trivedisarthak01@gmail.com>
2025-09-30 21:19:08 +0530

    1st commit

--- balance_paranthesis.java
+++ balance_paranthesis.java
@@ -1,3 +1,47 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+import java.util.Stack;
+
 public class balance_paranthesis {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String s = sc.nextLine();
+        int n = s.length();
+        ArrayList<Character> al = new ArrayList<>();
+        Stack<Character> st = new Stack<>();
+        for (int i=0;i<n;i++)
+            al.add(s.charAt(i));
+        for(int i=0;i<n;i++)
+        {
+            if(al.get(i)=='(' || al .get(i)=='{' || al.get(i)=='[')
+               {
+                 st.push(al.get(i));
+                 System.out.println("pushing in stack"+al.get(i));
+               }
+            else{
+                if(st.peek()=='[' && al.get(i)==']')
+                {
+                    st.pop();
+                }
+                else if(st.peek()=='(' && al.get(i)==')')
+                {
+                    st.pop();
+                }
+                 else if(st.peek()=='{' && al.get(i)=='}')
+                {
+                    st.pop();
+                }
+                else{
+                    System.out.println("Paranthesis are not balanced");
+                    System.exit(0);
+                }
+            }
+
+        }
+        if(st.empty())  System.out.println("Paranthesis are Balanced");
+
+       // else System.out.println("Paranthesis are not balanced");
+        
+    }
     
 }

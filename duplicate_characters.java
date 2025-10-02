c3779488f8237a2a0c076226656b22e880bd70db
Sarthaktrivedi4863 <trivedisarthak01@gmail.com>
2025-10-03 00:05:09 +0530

    coo

---
+++ duplicate_characters.java
@@ -0,0 +1,28 @@
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.Scanner;
+
+public class duplicate_characters {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String str = sc.nextLine();
+        HashSet hs = new HashSet<>();
+        ArrayList<Character> al = new ArrayList<>();
+        int n = str.length();
+        for(int i=0;i<n;i++)
+        {
+            if(!hs.contains(str.charAt(i)))
+            {
+                hs.add(str.charAt(i));
+            }
+            else{
+                al.add(str.charAt(i));
+            }
+        }
+
+        System.out.println("Repeated Characters are: ");
+        for(int i=0;i<n;i++)
+            System.out.print(al.get(i)+",");
+
+    }
+}
